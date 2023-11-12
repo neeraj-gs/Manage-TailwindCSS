567788f82daae058acb4de1b481ca26d0137a99e
neeraj-gs <gsneeraj05@gmail.com>
2023-11-12 13:08:06 +0530

    Added a componet sepereately for all compoents and seperate skeltetons

--- shoe-store/src/App.jsx
+++ shoe-store/src/App.jsx
@@ -1,30 +1,35 @@
+import {
+  Hero,PopularProducts,SuperQuality,Services,SpecialOffer,CustomerReviews,Subscribe,Footer
+} from './sections/index'
+
+
 const App = () => {
   return (
     <main className="relative">
       Nav 
       <section className="xl:padding-1 wide:padding-r padding-b">
-        Hero
+        <Hero />
       </section>
       <section className="padding">
-        Popular Products
+        <PopularProducts />
       </section>
       <section className="padding">
-        SuperQuality
+        <SuperQuality  />
       </section>
       <section className="padding-x py-10">
-        Services
+        <Services  />
       </section>
       <section className="padding">
-        Special Offer
+        <SpecialOffer  />
       </section>
       <section className="bg-pale-blue padding">
-        Customer Reviews 
+        <CustomerReviews />
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-        Subscribe
+        <Subscribe  />
       </section>
       <section className="bg-black padding-x padding-t pb-8">
-        Footer
+        <Footer  />
       </section>
 
     </main>
