3c9ef81dbebeb92fe227b2f816ec605e307bba7d
neeraj-gs <gsneeraj05@gmail.com>
2023-11-15 11:06:57 +0530

    Added a contsnats file that has the live data and creted a navbar sekelton adn added a logo on to the page

--- shoe-store/src/components/Nav.jsx
+++ shoe-store/src/components/Nav.jsx
@@ -1,10 +1,16 @@
+import hamburger from '../assets/icons/hamburger.svg'
+
 const Nav = () => {
   return (
-    <header>
-        <nav>
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'> 
             <a href="/">
-                
+                <img src="/shoe-shop.png" alt="header-logo"  width={130} height={29}/>
             </a>
+
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+              
+            </ul>
         </nav>
     </header>
   )
