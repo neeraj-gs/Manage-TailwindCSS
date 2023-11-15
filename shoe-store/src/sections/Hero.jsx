76f0ab722a4e1143eb7fc2493a2d4a04e988e63b
neeraj-gs <gsneeraj05@gmail.com>
2023-11-15 11:53:12 +0530

    Added some basic styles to the hero sectiona dn styled them for smaller devices

--- shoe-store/src/sections/Hero.jsx
+++ shoe-store/src/sections/Hero.jsx
@@ -6,18 +6,18 @@ const Hero = () => {
   return (
     <section id="home" className="w-full  flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>One Stop Solution to All Shoe Addicts</p>
-        <h1>
-          <span>The Newest Arrival </span> <br /> <span>Box</span>  Shoes</h1>
-          <p>Discover stylish SHoebox arrivals, best quality badn comfort and an innovation for your active life</p>
+        <p className="mt-20 text-xl font-montserrat text-coral-red">One Stop Solution to All Shoe Addicts</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The Newest Arrival </span> <br /> <span className="text-coral-red inline-block mt-3">Boxes In</span>  Town</h1>
+          <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish SHoebox arrivals, best quality badn comfort and an innovation for your active life</p>
           <Button label="Shop now" iconURL={arrowRight}/>
 
           <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
             {statistics.map((s)=>{
               return(
                 <div key={s.label}>
-                  <p>{s.value}</p>
-                  <p>{s.label}</p>
+                  <p className="text-2xl font-palanquin font-bold">{s.value}</p>
+                  <p className="leading-7 font-montserrat text-slate-gray">{s.label}</p>
                 </div>
               )
             })}
