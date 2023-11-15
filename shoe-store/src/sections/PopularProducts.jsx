7dec5712b8b42bb78c0f0b90e1ca069110bec6a3
neeraj-gs <gsneeraj05@gmail.com>
2023-11-15 13:49:14 +0530

    Added popular products cahgnes

--- shoe-store/src/sections/PopularProducts.jsx
+++ shoe-store/src/sections/PopularProducts.jsx
@@ -5,8 +5,8 @@ const PopularProducts = () => {
   return (
     <section id='products' className='max-container max-sm:mt-12'>
       <div className='flex flex-col justify-start gap-5'>
-        <h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Popular</span>  Products</h2>
-        <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>Experience the best quality shoes in town, with our most popular products. A Seperate world of comfort,design and value</p>
+        <h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Goto</span>  Products</h2>
+        <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>Experience the best quality shoes , in the most afforadable store in the town. Come Grap your Shoes and dont compromise on your footwears.</p>
       </div>
 
       <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14'>
